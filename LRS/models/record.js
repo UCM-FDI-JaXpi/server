649213567688724e6f1a59709bb73409257549d3
mcolomba <marc057>
2024-02-20 18:48:24 +0100

    Change record schema to store raw statements

--- LRS/models/record.js
+++ LRS/models/record.js
@@ -1,54 +1,48 @@
 const mongoose = require('mongoose');
 
 const recordSchema = new mongoose.Schema({
-    user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true
-    },
-    statement: {
-        actor: {
-            name: { type: String, required: true },
-            mbox: { type: String, required: true }
-        },
-        verb: {
-            id: { type: String, required: true },
-            display: { type: Object, required: true } 
-        },
-        object: {
-            id: { type: String, required: true },
-            definition: {
-                type: { type: String, required: true },
-                name: { type: Object, required: true }, 
-                description: { type: Object, required: true }, 
-                extensions: { type: Object } 
-            }
-        },
-        result: {
-            completion: { type: Boolean },
-            success: { type: Boolean },
-            score: {
-                scaled: { type: Number }
-            },
-            extensions: { type: Object } 
-        },
-        context: {
-            instructor: {
-                name: { type: String },
-                mbox: { type: String }
-            },
-            contextActivities: {
-                parent: { id: { type: String } },
-                grouping: { id: { type: String } }
-            },
-            extensions: { type: Object } 
-        },
-        timestamp: { type: Date, required: true },
-        stored: { type: Date, required: true },
-        authority: {
-            name: { type: String },
-            mbox: { type: String }
-        }
-    }
+	actor: {
+		name: { type: String, required: true },
+		mbox: { type: String, required: true }
+	},
+	verb: {
+		id: { type: String, required: true },
+		display: { type: Object, required: true }
+	},
+	object: {
+		id: { type: String, required: true },
+		definition: {
+			type: { type: String, required: true },
+			name: { type: Object, required: true },
+			description: { type: Object, required: true },
+			extensions: { type: Object }
+		}
+	},
+	result: {
+		completion: { type: Boolean },
+		success: { type: Boolean },
+		score: {
+			scaled: { type: Number }
+		},
+		extensions: { type: Object }
+	},
+	context: {
+		instructor: {
+			name: { type: String },
+			mbox: { type: String }
+		},
+		contextActivities: {
+			parent: { id: { type: String } },
+			grouping: { id: { type: String } }
+		},
+		extensions: { type: Object }
+	},
+	timestamp: { type: Date, required: true },
+	stored: { type: Date, required: true },
+	authority: {
+		name: { type: String },
+		mbox: { type: String }
+	}
 });
 
 module.exports = mongoose.model('Record', recordSchema);
