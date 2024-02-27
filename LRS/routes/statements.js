196e7829da53db268886c50c8d18be83dc383820
mcolomba <marc057>
2024-02-27 19:38:46 +0100

    Remove log auth

--- LRS/routes/statements.js
+++ LRS/routes/statements.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const Record = require('../models/record');
 const { checkAuthenticated } = require('../index');
 
-
 // Getting all statements
 router.get('/', checkAuthenticated, async (req, res) => {
 	try {
