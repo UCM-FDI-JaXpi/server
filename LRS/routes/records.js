fbcbd4fc1f1e8a1287f634527d15ef16dcbb1a02
mcolomba <marc057>
2024-03-09 16:57:21 +0100

    Modified get by userType

--- LRS/routes/records.js
+++ LRS/routes/records.js
@@ -1,28 +1,133 @@
 const express = require('express');
 const router = express.Router();
 const Record = require('../models/record');
-const { checkAuthenticated } = require('../index');
+const { checkAuthenticated, getUserType } = require('../index');
 
 // Getting all statements
 router.get('/', checkAuthenticated, async (req, res) => {
-	try {
-		const statement = await Record.find(); 
-		res.json(statement);
-	} catch {
-		res.status(500).json({ message: err.message });
+	const userType = getUserType(req);
+	if (userType === 'student') {
+		try {
+			const mbox = 'mailto:' + req.user.email;
+			console.log(mbox);
+			// Gets all statements from a student
+			const statements = await Record.find({
+				$or: [
+					{ 'actor.name': req.user.name },
+					{ 'actor.mbox': mbox }
+				]
+			});
+			res.json(statements);
+		} catch (err) {
+			res.status(500).json({ message: err.message });
+		}
+	}
+	else if (userType === 'teacher') {
+		try {
+			const mbox = 'mailto:' + req.user.email;
+			console.log(mbox);
+
+			/*
+			Get all statements of a teacher grouped by context.contextActivities.parent. 
+			Then it creates a field "actors", as a set ($addToSet) of all actor names, 
+			and saves it in the array statements.
+			*/
+			const statements = await Record.aggregate([
+				{
+					$match: {
+						$or: [
+							{ 'context.instructor.name': req.user.name },
+							{ 'context.instructor.mbox': mbox }
+						]
+					}
+				},
+				{
+					$group: {
+						_id: {
+							parent: '$context.contextActivities.parent',
+							studentName: '$actor.name'
+						},
+						statements: { $push: '$$ROOT' }
+					}
+				},
+				{
+					$group: {
+						_id: '$_id.parent',
+						actors: {
+							$push: {
+								studentName: '$_id.studentName',
+								statements: '$statements'
+							}
+						}
+					}
+				},
+				{
+					$project: {
+						_id: 1,
+						actors: 1
+					}
+				}
+			]);
+
+			res.json(statements);
+		} catch (err) {
+			res.status(500).json({ message: err.message });
+		}
+	}
+	else if (userType === 'dev') {
+		try {
+			const statements = await Record.aggregate([
+				{
+					$group: {
+						_id: {
+							parent: '$context.contextActivities.parent',
+							studentName: '$actor.name'
+						},
+						statements: { $push: '$$ROOT' }
+					}
+				},
+				{
+					$group: {
+						_id: '$_id.parent',
+						actors: {
+							$push: {
+								studentName: '$_id.studentName',
+								statements: '$statements'
+							}
+						}
+					}
+				},
+				{
+					$project: {
+						_id: 1,
+						actors: 1
+					}
+				}
+			]);
+			
+			// Falta "pseudonomizar" los resultados
+
+			res.json(statements);
+
+		} catch (err) {
+			res.status(500).json({ message: err.message });
+		}
+	}
+	else {
+		res.status(500).json({ message: 'Error finding user type' });
 	}
 });
 
 // Getting all statements of a user
 router.get('/:uid', checkAuthenticated, async (req, res) => {
-    const userId = req.params.uid;
+	const userId = req.params.uid;
 
-    try {
-        const statements = await Record.find({ user_id: userId });
-        res.json(statements);
-    } catch(err) {
-        res.status(500).json({ message: err.message });
-    }
+	try {
+		const statements = await Record.find({ user_id: userId });
+		res.json(statements);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
 });
 
 // Getting one statement by its ID
@@ -32,51 +137,47 @@ router.get('/:stid', checkAuthenticated, getStatementByID, (req, res) => {
 
 // Creating one statement
 router.post('/', async (req, res) => {
-	const statement = req.statement;
-	const user_id = req.user._id; // obtains user id from authenticated session
-	const record = new Record({
-		user_id: user_id,
-		statement: statement 
-	});
+	const statement = req.body;
+	const record = new Record(statement);
 
 	try {
 		const newRecord = await record.save();
 		res.status(201).json(newRecord); // 201 means succesfully created an object
-	} catch(err) {
-		res.status(400).json({ message: err.message }); 
+	} catch (err) {
+		res.status(400).json({ message: err.message });
 	}
 });
 
 // Updating one statement
 router.patch('/:id', checkAuthenticated, getStatementByID, async (req, res) => {
-	if(req.body.name != stll) {
+	if (req.body.name != stll) {
 		res.user.name = req.body.name;
 	}
 	try {
 		const updatedUser = await res.user.save();
 		res.json(updatedUser);
-	} catch(err) {
+	} catch (err) {
 		res.status(400).json({ message: err.message });
 	}
 });
 
 router.patch('/:stid', checkAuthenticated, getStatementByID, async (req, res) => {
-    const st = req.body.statement;
-    const uid = req.body.user_id;
-	
-    if (st != null) {
-        res.record.statement = st;
-    }
-	if (uid != null) {
-        res.record.user_id = uid;
-    }
+	const st = req.body.statement;
+	const uid = req.body.user_id;
 
-    try {
-        const updatedRecord = await res.record.save();
-        res.json(updatedRecord);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
+	if (st != null) {
+		res.record.statement = st;
+	}
+	if (uid != null) {
+		res.record.user_id = uid;
+	}
+
+	try {
+		const updatedRecord = await res.record.save();
+		res.json(updatedRecord);
+	} catch (err) {
+		res.status(400).json({ message: err.message });
+	}
 });
 
 // Deleting one statement
@@ -84,7 +185,7 @@ router.delete('/:stid', checkAuthenticated, getStatementByID, async (req, res) =
 	try {
 		await res.statement.deleteOne();
 		res.json({ message: 'Deleted statement' });
-	} catch(err) {
+	} catch (err) {
 		res.status(500).json({ message: err.message });
 	}
 });
@@ -97,7 +198,7 @@ async function getStatementByID(req, res, next) {
 		if (statement == null) {
 			return res.status(404).json({ message: 'Cannot find statement' });
 		}
-	} catch(err) {
+	} catch (err) {
 		return res.status(500).json({ message: err.message });
 	}
 
