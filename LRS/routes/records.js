f24081913b563d1451dad03dec08ed807c548c69
marc057 <macolomb@ucm.es>
2024-04-16 19:48:15 +0200

    Delete useless routes patch and delete

--- LRS/routes/records.js
+++ LRS/routes/records.js
@@ -139,7 +139,7 @@ router.get('/', checkAuthenticated, async (req, res) => {
 });
 
 // Getting all statements from a specific user
-router.get('/:uname', checkAuthenticated, getStatementByID, async (req, res) => {
+router.get('/:uname', checkAuthenticated, async (req, res) => {
 	const userType = getUserType(req);
 	if (userType === 'student') {
 		if (uname === req.user.name) {
@@ -182,11 +182,6 @@ router.get('/:uname', checkAuthenticated, getStatementByID, async (req, res) => 
 	}
 });
 
-// Getting one statement by its ID
-router.get('/:stid', checkAuthenticated, getStatementByID, (req, res) => {
-	res.send(res.statement);
-});
-
 // Creating one statement
 router.post('/', async (req, res) => {
 	const statement = req.body;
@@ -203,63 +198,5 @@ router.post('/', async (req, res) => {
 		res.status(400).json({ message: err.message });
 	}
 });
-
-// Updating one statement
-router.patch('/:id', checkAuthenticated, getStatementByID, async (req, res) => {
-	if (req.body.name != stll) {
-		res.user.name = req.body.name;
-	}
-	try {
-		const updatedUser = await res.user.save();
-		res.json(updatedUser);
-	} catch (err) {
-		res.status(400).json({ message: err.message });
-	}
-});
-
-router.patch('/:stid', checkAuthenticated, getStatementByID, async (req, res) => {
-	const st = req.body.statement;
-	const uid = req.body.user_id;
-
-	if (st != null) {
-		res.record.statement = st;
-	}
-	if (uid != null) {
-		res.record.user_id = uid;
-	}
-
-	try {
-		const updatedRecord = await res.record.save();
-		res.json(updatedRecord);
-	} catch (err) {
-		res.status(400).json({ message: err.message });
-	}
-});
-
-// Deleting one statement
-router.delete('/:stid', checkAuthenticated, getStatementByID, async (req, res) => {
-	try {
-		await res.statement.deleteOne();
-		res.json({ message: 'Deleted statement' });
-	} catch (err) {
-		res.status(500).json({ message: err.message });
-	}
-});
-
-// Middleware for specific user
-async function getStatementByID(req, res, next) {
-	let statement;
-	try {
-		statement = await Record.findById(req.params.stid);
-		if (statement == null) {
-			return res.status(404).json({ message: 'Cannot find statement' });
-		}
-	} catch (err) {
-		return res.status(500).json({ message: err.message });
-	}
-
-	res.statement = statement;
-	next();
-}
 
 module.exports = router;
