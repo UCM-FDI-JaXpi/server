3a8adb505b39f0064e76ed35cf3a852b91896583
marc057 <macolomb@ucm.es>
2024-07-07 19:16:41 +0200

    Fix minor bug

--- LRS/index.js
+++ LRS/index.js
@@ -21,7 +21,7 @@ app.use(flash());
 
 // Middleware personalizado para CORS permisivo
 const permissiveCorsMiddleware = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Credentials', 'true');
